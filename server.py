b3611593026a78edb4fdb8482d83c0eeb4aee532
Anton Krylov <anthony.kryloff@gmail.com>
2018-11-25 12:11:12 +0300

    fix web server

--- server.py
+++ server.py
@@ -8,12 +8,23 @@ with open("wall.txt") as f:
 
 text_model = markovify.Text(text, state_size=3)
 
+oh_model = markovify.Text(open('wall-overhear.txt').read(), state_size=3)
+
 
 def generate():
     # TODO
     return text_model.make_sentence(tries=1000)
 
 
+def generate_oh():
+    return oh_model.make_sentence(tries=1000)
+
+
 @app.route("/")
 def hello():
-    return render_template('index.html', joke=generate())
+    return render_template('index.html', joke=generate(), subj='юморесок')
+
+
+@app.route("/overhear")
+def oh():
+    return render_template('index.html', joke=generate_oh(), subj='подслушано')
