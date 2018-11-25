7eeec26d1e947ba564e7bbca3754775f1d0a1c6c
Anton Krylov <anthony.kryloff@gmail.com>
2018-11-25 12:02:00 +0300

    fix web server

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: FLASK_APP=server.py flask run --host=0.0.0.0
+web: FLASK_APP=server.py flask run -h 0.0.0.0 -p $PORT
--- server.py
+++ server.py
@@ -1,6 +1,14 @@
 from flask import Flask, render_template
+import markovify
 
 app = Flask(__name__)
+
+with open("wall.txt") as f:
+    text = f.read()
+
+text_model = markovify.Text(text, state_size=3)
+
+print(text_model.make_sentence(tries=1000))
 
 
 def generate():
